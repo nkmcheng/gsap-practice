1af40608102929e2177a9e06ea6e0ad29869e5b3
nkmcheng <nkmcheng@live.com>
2022-01-06 14:41:29 +0800

    finish part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { gsap, Power3 } from "gsap";
 
 function App() {
+  let logoRef = useRef(null);
+  let textRef = useRef(null);
+
+  console.log(logoRef);
+
+  useEffect(() => {
+    console.log("useEffect", logoRef);
+    // logoRef.style.display = "none";
+    gsap.to(logoRef, {
+      duration: 2,
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
+
+    gsap.to(textRef, {
+      duration: 2,
+      delay: 0.4,
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={(el) => {
+            logoRef = el;
+          }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p
+          ref={(el) => {
+            textRef = el;
+          }}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
